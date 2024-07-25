97795eeb011e45b7016c0e2839104bb3bc1e4d4e
Dmytro Neboha <dmytro.neboha@gmail.com>
2024-07-25 19:46:42 +0300

    Initial Commit

---
+++ Bizon/BizonApp.swift
@@ -0,0 +1,17 @@
+//
+//  BizonApp.swift
+//  Bizon
+//
+//  Created by Dmytro Neboha on 25.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct BizonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bizon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bizon
+//
+//  Created by Dmytro Neboha on 25.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
